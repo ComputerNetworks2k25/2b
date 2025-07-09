f848b2e95d1374f4e90dc9fab320686e0ec1a1d8
Abhishek <abhishekpatel46420@gmail.com>
2025-07-09 12:25:20 +0530

    final push

--- code.cpp
+++ code.cpp
@@ -2,16 +2,15 @@
 #include <string>
 using namespace std;
 
-// XOR operation between two binary strings
+
 string xorOperation(const string& a, const string& b) {
     string result = "";
-    for (size_t i = 0; i < a.length(); i++) {
+    for (int i = 0; i < a.length(); i++) {
         result += (a[i] == b[i]) ? '0' : '1';
     }
     return result;
 }
 
-// Performs CRC division and returns the remainder
 string crcDivision(string data, string polynomial) {
     int polyLen = polynomial.length();
     string rem = data.substr(0, polyLen);
@@ -25,24 +24,23 @@ string crcDivision(string data, string polynomial) {
         rem = rem.substr(1) + data[i];
     }
 
-    // Final step
     if (rem[0] == '1') {
         rem = xorOperation(rem, polynomial);
     } else {
         rem = xorOperation(rem, string(polyLen, '0'));
     }
 
-    return rem.substr(1);  // drop the first bit
+    return rem.substr(1);  
 }
 
-// Appends the CRC bits to the original data
 string appendCRC(string data, string polynomial) {
     string paddedData = data + string(polynomial.length() - 1, '0');
     string crc = crcDivision(paddedData, polynomial);
+    cout<<"Remainder is "<<crc<<endl;
     return data + crc;
 }
 
-// Checks if received data contains any errors
+
 bool detectError(string receivedData, string polynomial) {
     string rem = crcDivision(receivedData, polynomial);
     return (rem != string(polynomial.length() - 1, '0'));
@@ -57,11 +55,9 @@ int main() {
     cout << "Enter the polynomial (binary): ";
     cin >> polynomial;
 
-    // Sender side
     string dataWithCRC = appendCRC(data, polynomial);
     cout << "Data with CRC (transmitted): " << dataWithCRC << endl;
 
-    // Receiver side
     string receivedData;
     cout << "Enter received data (binary): ";
     cin >> receivedData;
